ab13801bf2f97a917611da1cfe205ff1ed8582fe
jubair hridoy <jubair_29@outlook.com>
2020-06-21 16:23:20 +0600

    save in db

--- Receipe/Controllers/HomeController.cs
+++ Receipe/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Receipe.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +11,79 @@ namespace Receipe.Controllers
 {
     public class HomeController : Controller
     {
+        private HMS_LIVEEntities db = new HMS_LIVEEntities();
         public ActionResult Index()
         {
-            return View();
+            rcp_viewmodel rcp_Viewmodel = new rcp_viewmodel();
+
+            List<ingredie> ingredieList = new List<ingredie>() {
+                new ingredie(){ id=1, ingredieName="Bill"},
+                new ingredie(){ id=2, ingredieName="Steve"},
+                new ingredie(){ id=3, ingredieName="Ram"},
+                new ingredie(){ id=4, ingredieName="Moin"}
+            };
+            List<unit> unitList = new List<unit>() {
+                new unit(){ id=1, unitName="kg"},
+                new unit(){ id=2, unitName="mg"},
+                new unit(){ id=3, unitName="litter"},
+                new unit(){ id=4, unitName="cm"}
+            };
+
+            ViewBag.IngredieList = ingredieList;
+            ViewBag.UnitList = unitList;
+
+            rcp_Viewmodel.ingredieList = ingredieList;
+            rcp_Viewmodel.unitList = unitList;
+
+
+            return View(rcp_Viewmodel);
         }
 
-        public ActionResult About()
+        
+        [HttpPost]
+        public ActionResult Index(rcp_viewmodel _rcp_Viewmodel,FormCollection form)
         {
-            ViewBag.Message = "Your application description page.";
+            rcp_viewmodel rcp_Viewmodel = new rcp_viewmodel();
 
-            return View();
+            List<ingredie> ingredieList = new List<ingredie>() {
+                new ingredie(){ id=1, ingredieName="Bill"},
+                new ingredie(){ id=2, ingredieName="Steve"},
+                new ingredie(){ id=3, ingredieName="Ram"},
+                new ingredie(){ id=4, ingredieName="Moin"}
+            };
+            List<unit> unitList = new List<unit>() {
+                new unit(){ id=1, unitName="kg"},
+                new unit(){ id=2, unitName="mg"},
+                new unit(){ id=3, unitName="litter"},
+                new unit(){ id=4, unitName="cm"}
+            };
+
+            ViewBag.IngredieList = ingredieList;
+            ViewBag.UnitList = unitList;
+
+            rcp_Viewmodel.ingredieList = ingredieList;
+            rcp_Viewmodel.unitList = unitList;
+
+            int IngredieId =Convert.ToInt32( form["IngredieList"].ToString());
+            int UnitID = Convert.ToInt32(form["UnitList"].ToString());
+
+            rcp_ingredients_costsheet_child_t rcp_Ingredients_Costsheet_Child_T = new rcp_ingredients_costsheet_child_t();
+            rcp_Ingredients_Costsheet_Child_T.ingredients_id = IngredieId;
+            rcp_Ingredients_Costsheet_Child_T.rct_ingredients_measurement_unit = UnitID;
+            rcp_Ingredients_Costsheet_Child_T.rcp_ingredients_costsheet_id = 1;
+            rcp_Ingredients_Costsheet_Child_T.rec_standard_cost = _rcp_Viewmodel.price;
+            rcp_Ingredients_Costsheet_Child_T.rec_standard_deviation_percentage = _rcp_Viewmodel.elements;
+
+             
+            
+                db.rcp_ingredients_costsheet_child_t.Add(rcp_Ingredients_Costsheet_Child_T);
+                db.SaveChanges();
+               
+            
+            return RedirectToAction("Index","IngredientsCostsheet");
+
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ Receipe/Models/rcp_viewmodel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Receipe.Models
+
+
+{
+    public  class ingredie
+    {
+
+      public  int id { get; set; }
+      public string ingredieName { get; set; }
+
+    }
+    public class unit
+    {
+
+        public int id { get; set; }
+        public string unitName { get; set; }
+
+    }
+    public class rcp_viewmodel
+    {
+        public List<ingredie> ingredieList { get; set; }
+        public List<unit> unitList { get; set; }
+
+        public decimal price { get; set; }
+
+        public int elements { get; set; }
+
+    }
+}
