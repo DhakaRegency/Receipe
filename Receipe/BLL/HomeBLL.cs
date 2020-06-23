96d8561c6d90a5842fc61579c933936290eed981
jubair hridoy <jubair_29@outlook.com>
2020-06-24 02:54:24 +0600

    Excaption

--- Receipe/BLL/HomeBLL.cs
+++ Receipe/BLL/HomeBLL.cs
@@ -9,8 +9,37 @@ namespace Receipe.BLL
     public class HomeBLL
     {
         private HMS_LIVEEntities db = new HMS_LIVEEntities();
-        
 
+        public List<rcp_viewmodel> getViewModelList()
+        {
+            try
+            {
+
+               List<rcp_viewmodel> rcp_Viewmodel = new List<rcp_viewmodel>{
+               new rcp_viewmodel { price = 0, elements = 0 ,fromDate=DateTime.Now,toDate=DateTime.Now},
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+               new rcp_viewmodel { price = 0, elements = 0 },
+           };
+                return rcp_Viewmodel;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
         public List<ingredie> getIngredieList()
         {
 
@@ -37,52 +66,87 @@ namespace Receipe.BLL
         }
         public int get_ingredients_costsheet_id()
         {
-            var costsheet_id = db.rcp_ingredients_costsheet_parent_t.OrderBy(x=>x.id).ToList().LastOrDefault().id;
-            return costsheet_id;
+            try
+            {
+                var costsheet_id = db.rcp_ingredients_costsheet_parent_t.OrderBy(x => x.id).ToList().LastOrDefault().id;
+                return costsheet_id;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
-        public bool isIngredientInserted(DateTime fromdate ,DateTime todate, int ingredientId)
+        public bool isIngredientInserted(DateTime fromdate, DateTime todate, int ingredientId)
         {
-            bool isTrue=false;
-            DateTime FromDate = Convert.ToDateTime(fromdate);
-            DateTime ToDate = Convert.ToDateTime(todate);
-            
-            int costsheetId;
-            var costsheet = db.rcp_ingredients_costsheet_parent_t.Where(m => m.effective_from_date == FromDate || m.effective_to_date == ToDate).Select(x => x.id).ToList();
-
-            int[] costSheetIdList = new int[costsheet.Count - 1]; 
-            for (int i = 0; i < costsheet.Count - 1; i++)
+            try
             {
-                int id = costsheet[i];
-                costsheetId = db.rcp_ingredients_costsheet_child_t.Where(m => m.rcp_ingredients_costsheet_id == id && m.ingredients_id == ingredientId).OrderBy(x => x.ingredients_id).Select(m => m.id).FirstOrDefault();
 
-                costSheetIdList[i]= costsheetId;
+                bool isTrue = false;
+                DateTime FromDate = Convert.ToDateTime(fromdate);
+                DateTime ToDate = Convert.ToDateTime(todate);
+
+                int costsheetId;
+                var costsheet = db.rcp_ingredients_costsheet_parent_t.Where(m => m.effective_from_date == FromDate || m.effective_to_date == ToDate).Select(x => x.id).ToList();
+
+                int[] costSheetIdList = new int[costsheet.Count - 1];
+                for (int i = 0; i < costsheet.Count - 1; i++)
+                {
+                    int id = costsheet[i];
+                    costsheetId = db.rcp_ingredients_costsheet_child_t.Where(m => m.rcp_ingredients_costsheet_id == id && m.ingredients_id == ingredientId).OrderBy(x => x.ingredients_id).Select(m => m.id).FirstOrDefault();
+
+                    costSheetIdList[i] = costsheetId;
+                }
+                if (costSheetIdList.Length != 0)
+                {
+                    isTrue = true;
+                }
+                return isTrue;
             }
-            if(costSheetIdList.Length!=0)
+            catch (Exception ex)
             {
-                isTrue = true;
+
+                throw ex;
             }
-             return isTrue;
         }
         public List<rcp_ingredients_costsheet_child_t> get_data()
         {
-            var costSheetList = db.rcp_ingredients_costsheet_child_t.OrderBy(x => x.id).ToList();
-            return costSheetList;
-        }
-        public List<rcp_ingredients_costsheet_child_t> get_data_within_date_range(string fromdate,string todate)
-        {
-            DateTime FromDate = Convert.ToDateTime(fromdate);
-            DateTime ToDate = Convert.ToDateTime(todate);
-            var costsheet =  db.rcp_ingredients_costsheet_parent_t.Where(m => m.effective_from_date >= FromDate || m.effective_to_date<=ToDate).Select(x=>x.id).ToList();
-            int costsheetId = 0;
-            if (costsheet.Count!=0)
-            { 
-                 costsheetId = costsheet[0];
-            }
-            
-            var costSheetList = db.rcp_ingredients_costsheet_child_t.Where(m=>m.rcp_ingredients_costsheet_id== costsheetId).OrderBy(x => x.id).ToList();
+            try
+            {
 
-            return costSheetList;
+                var costSheetList = db.rcp_ingredients_costsheet_child_t.OrderBy(x => x.id).ToList();
+                return costSheetList;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public List<rcp_ingredients_costsheet_child_t> get_data_within_date_range(string fromdate, string todate)
+        {
+            try
+            {
+
+                DateTime FromDate = Convert.ToDateTime(fromdate);
+                DateTime ToDate = Convert.ToDateTime(todate);
+                var costsheet = db.rcp_ingredients_costsheet_parent_t.Where(m => m.effective_from_date >= FromDate || m.effective_to_date <= ToDate).Select(x => x.id).ToList();
+                int costsheetId = 0;
+                if (costsheet.Count != 0)
+                {
+                    costsheetId = costsheet[0];
+                }
+
+                var costSheetList = db.rcp_ingredients_costsheet_child_t.Where(m => m.rcp_ingredients_costsheet_id == costsheetId).OrderBy(x => x.id).ToList();
+
+                return costSheetList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
     }
--- Receipe/Controllers/HomeController.cs
+++ Receipe/Controllers/HomeController.cs
@@ -18,114 +18,137 @@ namespace Receipe.Controllers
         static List<rcp_ingredients_costsheet_child_t> _rcp_ingredients_costsheet_child_t = new List<rcp_ingredients_costsheet_child_t>();
         public ActionResult Add()
         {
-            List<rcp_viewmodel> rcp_Viewmodel = new List<rcp_viewmodel>{
-               new rcp_viewmodel { price = 0, elements = 0 ,fromDate=DateTime.Now,toDate=DateTime.Now},
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-               new rcp_viewmodel { price = 0, elements = 0 },
-           };
+            try
+            {
 
+                List<rcp_viewmodel> rcp_Viewmodel = HomeBll.getViewModelList();
 
-            ViewBag.IngredieList = HomeBll.getIngredieList();
-            ViewBag.UnitList = HomeBll.getUnitList();
+                ViewBag.IngredieList = HomeBll.getIngredieList();
+                ViewBag.UnitList = HomeBll.getUnitList();
+                ViewBag.userId = new SelectList(HomeBll.getIngredieList(), "id", "ingredieName");
 
-            ViewBag.userId = new SelectList(HomeBll.getIngredieList(), "id", "ingredieName");
+                rcp_Viewmodel[0].ingredieList = HomeBll.getIngredieList();
+                rcp_Viewmodel[0].unitList = HomeBll.getUnitList();
 
-            rcp_Viewmodel[0].ingredieList = HomeBll.getIngredieList();
-            rcp_Viewmodel[0].unitList = HomeBll.getUnitList();
+                return View(rcp_Viewmodel);
+            }
+            catch (Exception ex)
+            {
 
-
-            return View(rcp_Viewmodel);
+                throw ex;
+            }
         }
 
 
         [HttpPost]
         public ActionResult Add(List<rcp_viewmodel> _rcp_Viewmodel, List<FormCollection> form)
         {
-            rcp_viewmodel rcp_Viewmodel = new rcp_viewmodel();
-
-            ViewBag.IngredieList = HomeBll.getIngredieList();
-            ViewBag.UnitList = HomeBll.getUnitList();
-
-            rcp_Viewmodel.ingredieList = HomeBll.getIngredieList();
-            rcp_Viewmodel.unitList = HomeBll.getUnitList();
-
-
-            string[] IngredieId = form[0]["ingredieList"].ToString().Split(',');
-            string[] UnitID = form[1]["UnitList"].ToString().Split(',');
-
-            rcp_ingredients_costsheet_parent_t rcp_Ingredients_Costsheet_Parent_T = new rcp_ingredients_costsheet_parent_t();
-            rcp_Ingredients_Costsheet_Parent_T.effective_from_date = _rcp_Viewmodel[0].fromDate;
-            rcp_Ingredients_Costsheet_Parent_T.effective_to_date = _rcp_Viewmodel[0].toDate;
-
-            db.rcp_ingredients_costsheet_parent_t.Add(rcp_Ingredients_Costsheet_Parent_T);
-            db.SaveChanges();
-
-            int costsheet_id = HomeBll.get_ingredients_costsheet_id();
-            for (int i = 0; i < 4; i++)
+            try
             {
-                if (!(HomeBll.isIngredientInserted(_rcp_Viewmodel[0].fromDate, _rcp_Viewmodel[0].toDate, Convert.ToInt32(IngredieId[i]))))
+                rcp_viewmodel rcp_Viewmodel = new rcp_viewmodel();
+
+                ViewBag.IngredieList = HomeBll.getIngredieList();
+                ViewBag.UnitList = HomeBll.getUnitList();
+
+                rcp_Viewmodel.ingredieList = HomeBll.getIngredieList();
+                rcp_Viewmodel.unitList = HomeBll.getUnitList();
+
+
+                string[] IngredieId = form[0]["ingredieList"].ToString().Split(',');
+                string[] UnitID = form[1]["UnitList"].ToString().Split(',');
+
+                rcp_ingredients_costsheet_parent_t rcp_Ingredients_Costsheet_Parent_T = new rcp_ingredients_costsheet_parent_t();
+                rcp_Ingredients_Costsheet_Parent_T.effective_from_date = _rcp_Viewmodel[0].fromDate;
+                rcp_Ingredients_Costsheet_Parent_T.effective_to_date = _rcp_Viewmodel[0].toDate;
+
+                db.rcp_ingredients_costsheet_parent_t.Add(rcp_Ingredients_Costsheet_Parent_T);
+                db.SaveChanges();
+
+                int costsheet_id = HomeBll.get_ingredients_costsheet_id();
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!(HomeBll.isIngredientInserted(_rcp_Viewmodel[0].fromDate, _rcp_Viewmodel[0].toDate, Convert.ToInt32(IngredieId[i]))))
                     {
-                    rcp_ingredients_costsheet_child_t rcp_Ingredients_Costsheet_Child_T = new rcp_ingredients_costsheet_child_t();
-                    rcp_Ingredients_Costsheet_Child_T.ingredients_id = Convert.ToInt32(IngredieId[i]);
-                    rcp_Ingredients_Costsheet_Child_T.rct_ingredients_measurement_unit = Convert.ToInt32(UnitID[i]);
-                    rcp_Ingredients_Costsheet_Child_T.rcp_ingredients_costsheet_id = costsheet_id;
-                    rcp_Ingredients_Costsheet_Child_T.rec_standard_cost = _rcp_Viewmodel[i].price;
-                    rcp_Ingredients_Costsheet_Child_T.rec_standard_deviation_percentage = _rcp_Viewmodel[i].elements;
-                    db.rcp_ingredients_costsheet_child_t.Add(rcp_Ingredients_Costsheet_Child_T);
-                    db.SaveChanges();
+                        rcp_ingredients_costsheet_child_t rcp_Ingredients_Costsheet_Child_T = new rcp_ingredients_costsheet_child_t();
+                        rcp_Ingredients_Costsheet_Child_T.ingredients_id = Convert.ToInt32(IngredieId[i]);
+                        rcp_Ingredients_Costsheet_Child_T.rct_ingredients_measurement_unit = Convert.ToInt32(UnitID[i]);
+                        rcp_Ingredients_Costsheet_Child_T.rcp_ingredients_costsheet_id = costsheet_id;
+                        rcp_Ingredients_Costsheet_Child_T.rec_standard_cost = _rcp_Viewmodel[i].price;
+                        rcp_Ingredients_Costsheet_Child_T.rec_standard_deviation_percentage = _rcp_Viewmodel[i].elements;
+                        db.rcp_ingredients_costsheet_child_t.Add(rcp_Ingredients_Costsheet_Child_T);
+                        db.SaveChanges();
+                    }
+
                 }
-                 
+                return RedirectToAction("Index", "IngredientsCostsheet");
             }
-            return RedirectToAction("Index", "IngredientsCostsheet");
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
 
         public ActionResult Update()
         {
+            try
+            {
+                ViewBag.IngredieList = HomeBll.getIngredieList();
+                ViewBag.UnitList = HomeBll.getUnitList();
 
-            ViewBag.IngredieList = HomeBll.getIngredieList();
-            ViewBag.UnitList = HomeBll.getUnitList();
+                return View(_rcp_ingredients_costsheet_child_t);
+            }
+            catch (Exception ex)
+            {
 
-            return View(_rcp_ingredients_costsheet_child_t);
+                throw;
+            }
+
         }
 
         [HttpPost]
         public ActionResult Update(List<rcp_ingredients_costsheet_child_t> rcp_Ingredients_Costsheet_Child_Ts)
         {
-
-            ViewBag.IngredieList = HomeBll.getIngredieList();
-            ViewBag.UnitList = HomeBll.getUnitList();
-            foreach (var item in rcp_Ingredients_Costsheet_Child_Ts)
+            try
             {
-                db.Entry(item).State = EntityState.Modified;
-                db.SaveChanges();
+                ViewBag.IngredieList = HomeBll.getIngredieList();
+                ViewBag.UnitList = HomeBll.getUnitList();
+                foreach (var item in rcp_Ingredients_Costsheet_Child_Ts)
+                {
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "IngredientsCostsheet");
             }
-            return RedirectToAction("Index", "IngredientsCostsheet");
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
 
         public ActionResult getCostSheet(FormCollection form)
         {
-            ViewBag.IngredieList = HomeBll.getIngredieList();
-            ViewBag.UnitList = HomeBll.getUnitList();
+            try
+            {
+                ViewBag.IngredieList = HomeBll.getIngredieList();
+                ViewBag.UnitList = HomeBll.getUnitList();
 
-            string fromdate = form[0].ToString();
-            string todate = form[1].ToString();
+                string fromdate = form[0].ToString();
+                string todate = form[1].ToString();
 
-            _rcp_ingredients_costsheet_child_t = HomeBll.get_data_within_date_range(fromdate, todate);
-            return RedirectToAction("Update", "Home", _rcp_ingredients_costsheet_child_t);
+                _rcp_ingredients_costsheet_child_t = HomeBll.get_data_within_date_range(fromdate, todate);
+                return RedirectToAction("Update", "Home", _rcp_ingredients_costsheet_child_t);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
         }
     }
 }
